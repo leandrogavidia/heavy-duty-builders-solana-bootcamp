78f252844e171f27df424f1ceeb782c553f355f9
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-02-15 23:30:40 -0400

    Chore: remove unnecesary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'heavy-duty-builders-solana-bootcamp-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+  <header>
+    <h1>Hi, I'm Bob</h1>
+  </header>`,
 })
 export class AppComponent {
   title = 'heavy-duty-builders-solana-bootcamp';
