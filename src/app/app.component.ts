7f0148705e318bd1794481a0077d72820843ecc4
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-02-15 23:48:41 -0400

    feat: integrate wallet-adapter library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'heavy-duty-builders-solana-bootcamp-root',
   template: `
   <header>
     <h1>Hi, I'm Bob</h1>
+    <hd-wallet-multi-button></hd-wallet-multi-button>
   </header>`,
 })
 export class AppComponent {
-  title = 'heavy-duty-builders-solana-bootcamp';
+  title = 'Heavy Duty Builders Solana Bootcamp';
 }
