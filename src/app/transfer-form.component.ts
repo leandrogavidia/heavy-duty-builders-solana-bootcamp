a322a0764613975d4d3ce7c0221707df4488ca98
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-02-29 21:05:59 -0400

    feat: disable form when the transaction is being sent

--- src/app/transfer-form.component.ts
+++ src/app/transfer-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, inject } from "@angular/core";
+import { Component, EventEmitter, Output, inject, input } from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
 import { MatButton } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
@@ -28,114 +28,118 @@ export interface TransferFormPayload {
         <form 
             #form="ngForm" 
             (ngSubmit)="onSubmitForm(form)"
-            class="flex flex-col justify-center items-center gap-y-3"
         >
-            <mat-form-field appearance="fill">
-                <mat-label>Concepto</mat-label>
-                <input 
-                    name="memo"
-                    type="text"
-                    matInput 
-                    placeholder="Ej. Pagar el recibo de electricidad"
-                    [(ngModel)]="model.memo"
-                    required
-                    #memoControl="ngModel"
-                >
-                <mat-icon matSuffix>description</mat-icon>
-
-                @if (form.submitted && memoControl.errors) {
-                    <mat-error>
-                        @if (memoControl.errors["required"]) {
-                            El Concepto es obligatorio.
-                        }
-                    </mat-error>
-                } @else {
-                    <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
-                }
-            </mat-form-field>
-
-            <mat-form-field appearance="fill">
-                <mat-label>Token</mat-label>
-                <select 
-                    name="token" 
-                    placeholder="ej. Silly"
-                    [(ngModel)]="model.token"
-                    required
-                    #tokenControl="ngModel"
-                    matNativeControl
-                >
-                    @for (token of tokenList(); track token.address) {
-                        <option [value]="token.address">{{ token.info.name }} | {{ token.info.symbol }}</option>
-                    }
-                </select>   
-                
-                <mat-icon matSuffix>attach_money</mat-icon>
-
-                @if (form.submitted && memoControl.errors) {
-                    <mat-error>
-                        @if (tokenControl.errors) {
-                            @if (tokenControl.errors["required"]) {
-                                El Token es obligatorio.
+            <fieldset
+                class="flex flex-col justify-center items-center gap-y-3"
+                [disabled]="this.isDisabled()"
+            >
+                <mat-form-field appearance="fill">
+                    <mat-label>Concepto</mat-label>
+                    <input 
+                        name="memo"
+                        type="text"
+                        matInput 
+                        placeholder="Ej. Pagar el recibo de electricidad"
+                        [(ngModel)]="model.memo"
+                        required
+                        #memoControl="ngModel"
+                    >
+                    <mat-icon matSuffix>description</mat-icon>
+    
+                    @if (form.submitted && memoControl.errors) {
+                        <mat-error>
+                            @if (memoControl.errors["required"]) {
+                                El Concepto es obligatorio.
                             }
+                        </mat-error>
+                    } @else {
+                        <mat-hint>Debe ser el motivo de la transferencia</mat-hint>
+                    }
+                </mat-form-field>
+    
+                <mat-form-field appearance="fill">
+                    <mat-label>Token</mat-label>
+                    <select 
+                        name="token" 
+                        placeholder="ej. Silly"
+                        [(ngModel)]="model.token"
+                        required
+                        #tokenControl="ngModel"
+                        matNativeControl
+                    >
+                        @for (token of tokenList(); track token.address) {
+                            <option [value]="token.address">{{ token.info.name }} | {{ token.info.symbol }}</option>
                         }
-                    </mat-error>
-                } @else {
-                    <mat-hint>Debe ser el token de la transferencia</mat-hint>
-                }
-            </mat-form-field>
-
-            <mat-form-field appearance="fill">
-                <mat-label>Monto</mat-label>
-                <input 
-                    name="amount"
-                    type="number"
-                    matInput
-                    min="0"
-                    placeholder="Ej. 15"
-                    [(ngModel)]="model.amount"
-                    required
-                    #amountControl="ngModel"
-                >
-                <mat-icon matSuffix>attach_money</mat-icon>
-
-                @if (form.submitted && amountControl.errors) {
-                    <mat-error>
-                        @if (amountControl.errors["required"]) {
-                            El monto es obligatorio.
-                        } @else if (amountControl.errors["mint"]) {
-                            El monto debe ser mayor a 0
-                        }
-                    </mat-error>
-                } @else {
-                    <mat-hint>Debe ser un monto mayor a 0</mat-hint>
-                }
-            </mat-form-field>
-
-            <mat-form-field appearance="fill">
-                <mat-label>Destinatario</mat-label>
-                <input 
-                    name="receiverAddress"
-                    type="text"
-                    matInput 
-                    placeholder="Ej. Wallet del destinatario"
-                    [(ngModel)]="model.receiverAddress"
-                    required
-                    #receiverAddressControl="ngModel"
-                >
-                <mat-icon matSuffix>key</mat-icon>
-
-                @if (form.submitted && receiverAddressControl.errors) {
-                    <mat-error>
-                        @if (receiverAddressControl.errors["required"]) {
-                            El La wallet del destinatario es obligatoria.
-                        }
-                    </mat-error>
-                } @else {
-                    <mat-hint>Wallet del destinatario en la transferencia</mat-hint>
-                }
-            </mat-form-field>
-
-            <button type="submit">Enviar</button>
+                    </select>   
+                    
+                    <mat-icon matSuffix>attach_money</mat-icon>
+    
+                    @if (form.submitted && memoControl.errors) {
+                        <mat-error>
+                            @if (tokenControl.errors) {
+                                @if (tokenControl.errors["required"]) {
+                                    El Token es obligatorio.
+                                }
+                            }
+                        </mat-error>
+                    } @else {
+                        <mat-hint>Debe ser el token de la transferencia</mat-hint>
+                    }
+                </mat-form-field>
+    
+                <mat-form-field appearance="fill">
+                    <mat-label>Monto</mat-label>
+                    <input 
+                        name="amount"
+                        type="number"
+                        matInput
+                        min="0"
+                        placeholder="Ej. 15"
+                        [(ngModel)]="model.amount"
+                        required
+                        #amountControl="ngModel"
+                    >
+                    <mat-icon matSuffix>attach_money</mat-icon>
+    
+                    @if (form.submitted && amountControl.errors) {
+                        <mat-error>
+                            @if (amountControl.errors["required"]) {
+                                El monto es obligatorio.
+                            } @else if (amountControl.errors["mint"]) {
+                                El monto debe ser mayor a 0
+                            }
+                        </mat-error>
+                    } @else {
+                        <mat-hint>Debe ser un monto mayor a 0</mat-hint>
+                    }
+                </mat-form-field>
+    
+                <mat-form-field appearance="fill">
+                    <mat-label>Destinatario</mat-label>
+                    <input 
+                        name="receiverAddress"
+                        type="text"
+                        matInput 
+                        placeholder="Ej. Wallet del destinatario"
+                        [(ngModel)]="model.receiverAddress"
+                        required
+                        #receiverAddressControl="ngModel"
+                    >
+                    <mat-icon matSuffix>key</mat-icon>
+    
+                    @if (form.submitted && receiverAddressControl.errors) {
+                        <mat-error>
+                            @if (receiverAddressControl.errors["required"]) {
+                                El La wallet del destinatario es obligatoria.
+                            }
+                        </mat-error>
+                    } @else {
+                        <mat-hint>Wallet del destinatario en la transferencia</mat-hint>
+                    }
+                </mat-form-field>
+    
+                <button type="submit">Enviar</button>
+            </fieldset>
         </form>
     `,
     standalone: true,
@@ -149,6 +153,8 @@ export interface TransferFormPayload {
 })
 
 export class TransferFormComponent {
+    readonly isDisabled = input.required<boolean>();
+
     readonly model: TransferFormModel = {
         memo: null,
         amount: null,
--- src/app/transfer-modal.component.ts
+++ src/app/transfer-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { TransferFormComponent, TransferFormPayload } from "./transfer-form.component";
 import { injectTransactionSender } from "@heavy-duty/wallet-adapter";
 import { createTransferInstructions } from "@heavy-duty/spl-utils";
@@ -8,7 +8,10 @@ import { createTransferInstructions } from "@heavy-duty/spl-utils";
     template: `
         <div class="p-4">
             <h2 class="mb-4">Transferir fondos</h2>
-            <heavy-duty-builders-solana-bootcamp-transfer-form (submitForm)="onTransfer($event)"></heavy-duty-builders-solana-bootcamp-transfer-form>
+            <heavy-duty-builders-solana-bootcamp-transfer-form 
+                (submitForm)="onTransfer($event)"
+                [isDisabled]="this.isRunning()"
+            ></heavy-duty-builders-solana-bootcamp-transfer-form>
         </div>
     `,
     standalone: true,
@@ -17,6 +20,16 @@ import { createTransferInstructions } from "@heavy-duty/spl-utils";
 
 export class TransferModalComponent {
     private readonly _transactionSender = injectTransactionSender();
+    private readonly _transactionStatus = computed(() => this._transactionSender().status);
+
+    readonly isRunning = computed(() =>
+        this._transactionStatus() === "confirming" ||
+        this._transactionStatus() === "finalizing" ||
+        this._transactionStatus() === "confirming"
+    )
+
+    readonly isSuccess = computed(() => this._transactionStatus() === "finalized");
+    readonly isFailed = computed(() => this._transactionStatus() === "failed")
 
     onTransfer(payload: TransferFormPayload) {
         this._transactionSender.send(({ publicKey }) => createTransferInstructions({
@@ -29,7 +42,7 @@ export class TransferModalComponent {
         })).subscribe({
             next: (signature) => console.log(`Firma: ${signature}`),
             error: (error) => console.error(error),
-            complete: () => console.log("Transaction lista")
+            complete: () => console.log("Transaction lista"),
         })
     }
 }
